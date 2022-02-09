92b0a01be3825b7b741e7aa8d31502f3edec42ac
Tunahan Ozatac <ExtTunahanO@d-teknoloji.com.tr>
2022-02-09 16:20:14 +0300

    Created Project

--- Swift-DarkMode/ViewController.swift
+++ Swift-DarkMode/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //overrideUserInterfaceStyle = .dark
+        //İstediimiz ViewController için tema tanımlaması yapabilriiz
+        //Yada Info dosyası içerisinden Apperance belirtip yada User Interface Style belirtip tema seçebiliriz.
     }
-
-
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userInterFaceStyle = traitCollection.userInterfaceStyle
+        if userInterFaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        }else{
+            changeButton.tintColor = UIColor.blue
+        }
+    }
 }
-
